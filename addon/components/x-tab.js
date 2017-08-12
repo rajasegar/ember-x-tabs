5db0a845678ae56952c15c3db904e2fbb8bc6b2c
Rajasegar Chandran <rajasegar.c@gmail.com>
2017-08-12 12:39:09 +0530

    Added ember-cli-github-pages addon

--- addon/components/x-tab.js
+++ addon/components/x-tab.js
@@ -4,44 +4,44 @@ import ComponentParent from '../mixins/component-parent';
 import TabPane from './x-tab/pane';
 
 const {
-    computed,
-    A,
-    getWithDefault
+  computed,
+  A,
+  getWithDefault
 } = Ember;
 
 export default Ember.Component.extend(ComponentParent, {
-    layout,
-    classNames:['tabs'],
-    classNameBindings: ['tab-style'],
-    'tab-style':'tabs-style-bar', // Default tab style
+  layout,
+  classNames:['tabs'],
+  classNameBindings: ['tab-style', 'customClass'],
+  // 'tab-style':'tabs-style-bar', // Default tab style
 
-    childPanes: computed.filter('children', function(view) {
-        return view instanceof TabPane;
-    }),
+  childPanes: computed.filter('children', function(view) {
+    return view instanceof TabPane;
+  }),
 
-    activeId: computed.oneWay('childPanes.firstObject.elementId'),
+  activeId: computed.oneWay('childPanes.firstObject.elementId'),
 
-    isActiveId: computed('activeId',  {
-        get() {
-            return getWithDefault(this, 'activeId', null);
-        },
-        set(key, value) {
-            return value;
-        }
-    }),
-
-    navItems: computed('childPanes.@each.{elementId,title,icon}', function() {
-        let items = A();
-        this.get('childPanes').forEach((pane) => {
-            let item = pane.getProperties('elementId', 'title', 'icon');
-            items.push(item);
-        });
-        return items;
-    }),
-
-    actions: {
-        select(id) {
-            this.set('isActiveId', id);
-        }
+  isActiveId: computed('activeId',  {
+    get() {
+        return getWithDefault(this, 'activeId', null);
+    },
+    set(key, value) {
+        return value;
     }
+  }),
+
+  navItems: computed('childPanes.@each.{elementId,title,icon}', function() {
+    let items = A();
+    this.get('childPanes').forEach((pane) => {
+        let item = pane.getProperties('elementId', 'title', 'icon');
+        items.push(item);
+    });
+    return items;
+  }),
+
+  actions: {
+    select(id) {
+      this.set('isActiveId', id);
+    }
+  }
 });
