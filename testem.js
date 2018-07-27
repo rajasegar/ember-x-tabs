0ef92f4b68a52486df6dde6ecf74c61d2797194f
Rajasegar Chandran <rajasegar.c@gmail.com>
2018-07-27 07:15:02 +0530

    Fixing test runs

--- testem.js
+++ testem.js
@@ -1,18 +1,29 @@
 /* eslint-env node */
 module.exports = {
-  test_page: 'tests/index.html?hidepassed',
+test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
   launch_in_ci: [
-    'Chrome'
+    'Chrome',
+    'Firefox'
   ],
   launch_in_dev: [
-    'PhantomJS',
     'Chrome'
   ],
   browser_args: {
     Chrome: {
       mode: 'ci',
-      args: [ '-headless' ]
+      args: [
+        '--disable-gpu',
+        '--headless',
+        '--remote-debugging-port=0',
+        '--window-size=1440,900'
+      ]
+    },
+    Firefox: {
+      mode: 'ci',
+      args: [
+        '-headless',
+      ]
     }
   }
 };
