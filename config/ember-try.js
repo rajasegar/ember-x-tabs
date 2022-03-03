b3a4635d7c3b8916f63c34ad9ec80136055d4615
Rajasegar <rajasegar.c@gmail.com>
2022-03-03 14:19:20 +0530

    chore: change demo page to contextual components
    1. Remove travis
    2. Add github actions
    3. change license date
    4. Remove old configs from ember-try

--- config/ember-try.js
+++ config/ember-try.js
@@ -12,18 +12,6 @@ module.exports = function() {
       useYarn: true,
       scenarios: [
         {
-          name: 'ember-lts-2.18',
-          env: {
-            EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true })
-          },
-          npm: {
-            devDependencies: {
-              '@ember/jquery': '^0.5.1',
-              'ember-source': '~2.18.0'
-            }
-          }
-        },
-        {
           name: 'ember-lts-3.4',
           npm: {
             devDependencies: {
@@ -39,22 +27,6 @@ module.exports = function() {
             }
           }
         },
-        {
-          name: 'ember-beta',
-          npm: {
-            devDependencies: {
-              'ember-source': urls[1]
-            }
-          }
-        },
-        {
-          name: 'ember-canary',
-          npm: {
-            devDependencies: {
-              'ember-source': urls[2]
-            }
-          }
-        },
         // The default `.travis.yml` runs this scenario via `yarn test`,
         // not via `ember try`. It's still included here so that running
         // `ember try:each` manually or from a customized CI config will run it
@@ -65,19 +37,6 @@ module.exports = function() {
             devDependencies: {}
           }
         },
-        {
-          name: 'ember-default-with-jquery',
-          env: {
-            EMBER_OPTIONAL_FEATURES: JSON.stringify({
-              'jquery-integration': true
-            })
-          },
-          npm: {
-            devDependencies: {
-              '@ember/jquery': '^0.5.1'
-            }
-          }
-        }
       ]
     };
   });
