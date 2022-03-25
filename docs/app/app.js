093556eb8640963443ab7575bdff4c7511c0d0e1
Brian Gantzler <brian.gantzler@cardinalhealth.com>
2022-03-25 11:28:17 -0400

    Updated documentation on how to include themes

--- docs/app/app.js
+++ docs/app/app.js
@@ -2,6 +2,8 @@ import Application from "@ember/application";
 import Resolver from "ember-resolver";
 import loadInitializers from "ember-load-initializers";
 import config from "docs/config/environment";
+import "ember-x-tabs/base.css";
+import "ember-x-tabs/all-themes.js";
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
